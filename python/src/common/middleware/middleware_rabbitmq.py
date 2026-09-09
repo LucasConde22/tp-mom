7c6ca69d8368d984942f9067b6df4056bb00c8cd
Lucas Ariel Conde Cardó <conde.cardo.lucas@gmail.com>
2026-09-08 21:01:22 -0300

    Creación y cierre de conexión de exchange

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,3 +1,5 @@
+from multiprocessing.dummy import connection
+
 import pika
 import random
 import string
@@ -11,4 +13,21 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+        self.routing_keys = routing_keys
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(
+            exchange=exchange_name,
+        )
+
+    def start_consuming(self, on_message_callback):
         pass
+
+    def stop_consuming(self):
+        pass
+
+    def send(self, message):
+        pass
+
+    def close(self):
+        self.connection.close()
